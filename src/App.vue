7fa2058d341b859c6936687a150170b084e56707
KingNelx <tapiajonel606@gmail.com>
2025-04-30 08:51:32 +0800

    Initial commit for Event Management App

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-blue-100">
+    <div class="container mt-5">
+      <h1 class="mb-4 text-center text-3xl font-bold">Event Management App</h1>
+
+      <AddEvent :events="events" />
+      <EventList :events="events" />
+    </div>
+  </div>
+</template>
+
+<script>
+import AddEvent from './components/AddEvent.vue'
+import EventList from './components/EventList.vue'
+import eventsData from './data/events.json'
+
+export default {
+  components: {
+    AddEvent,
+    EventList
+  },
+  data() {
+    return {
+      events: eventsData.map(event => ({
+        ...event,
+        likes: 0
+      }))
+    }
+  }
+}
+</script>
